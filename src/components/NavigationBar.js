d1b72d0997aceb7f6726d5a640f073cac5c924a0
NastiaDnk <dankoanastasia456@gmail.com>
2025-12-09 07:34:12 +0200

    removed nav link from router

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,38 +1,73 @@
+import { useRef } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
 import { PossibleRisksSources } from '../components/PossibleRisksSources'
 import { PotentialRiskEvents } from '../components/PotentialRiskEvents'
 import { RiskAnalysis } from '../components/RiskAnalysis'
 import { RiskPlanning } from '../components/RiskPlanning'
 import { RiskMonitoring } from '../components/RiskMonitoring'
-import { Routes, Route, Link } from 'react-router-dom';
 
-export const NavigationBar = () => { 
-    return(
-        <>
-            <Navbar bg="dark" variant={"dark"} expand="lg">
+export const NavigationBar = () => {
+
+    const navRef = useRef();
+    const navigate = useNavigate();
+
+    function redirect(to) {
+        return function () {
+            navigate(to);
+        }
+    }
+
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" ref={navRef}>
                 <Container>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link className='px-5 color-active' as={Link} to="/possible_risks_sources">Можливі джерела ризиків</Nav.Link>
-                            <Nav.Link className='px-5 color-active' as={Link} to="/potential_risk_events">Потенційні ризикові події</Nav.Link>
-                            <Nav.Link className='px-5 color-active' as={Link} to="/risk_analysis">Аналіз ризиків</Nav.Link>
-                            <Nav.Link className='px-5 color-active' as={Link} to="/risk_planning">Планування ризиків</Nav.Link>
-                            <Nav.Link className='px-5 color-active' as={Link} to="/risk_monitoring">Моніторинг ризиків</Nav.Link>
+
+                            <a className="nav-link px-5 color-active" onClick={redirect("/possible_risks_sources")}>
+                                Можливі джерела ризиків
+                            </a>
+
+                            <a className="nav-link px-5 color-active" onClick={redirect("/potential_risk_events")}>
+                                Потенційні ризикові події
+                            </a>
+
+                            <a className="nav-link px-5 color-active" onClick={redirect("/risk_analysis")}>
+                                Аналіз ризиків
+                            </a>
+
+                            <a className="nav-link px-5 color-active" onClick={redirect("/risk_planning")}>
+                                Планування ризиків
+                            </a>
+
+                            <a className="nav-link px-5 color-active" onClick={redirect("/risk_monitoring")}>
+                                Моніторинг ризиків
+                            </a>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <Routes>
-                <Route path="/" element={<PossibleRisksSources/>}/> 
-                <Route path="/possible_risks_sources" element={<PossibleRisksSources/>}/>
-                <Route path="/potential_risk_events" element={<PotentialRiskEvents/>}/>
-                <Route path="/risk_analysis" element={<RiskAnalysis/>}/>
-                <Route path="/risk_planning" element={<RiskPlanning/>}/>
-                <Route path="/risk_monitoring" element={<RiskMonitoring/>}/>
-            </Routes>
-        </>
+
+            <div>
+                <Routes>
+                    <Route path="/" element={<PossibleRisksSources />} />
+                    <Route path="/possible_risks_sources" element={<PossibleRisksSources />} />
+                    <Route path="/potential_risk_events" element={<PotentialRiskEvents />} />
+                    <Route path="/risk_analysis" element={<RiskAnalysis />} />
+                    <Route path="/risk_planning" element={<RiskPlanning />} />
+                    <Route path="/risk_monitoring" element={<RiskMonitoring />} />
+                    <Route path="*" element={<Navigate replace to="/" />} />
+                </Routes>
+            </div>
+        </header>
     );
-}
+};
